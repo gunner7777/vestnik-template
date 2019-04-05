87bcd07d3ba9f60f4c3ad55b2345baa2d9fcbcb7
gunner7777 <123@yandex.ru>
2019-04-05 09:54:06 +0300

    update scss. new begining

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,22 @@
-window.FontAwesomeConfig = {
+/* window.FontAwesomeConfig = {
   searchPseudoElements: true
-}
+} */
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.addEventListener('click', function (event) {
+    /*  if (!event.target.classList.contains('Search-Form') && document.querySelector('.Search-Form').classList.contains('Search-Form_Disabled')) {
+
+       document.querySelector('.Search-Form').classList.add('Search-Form_Disabled');
+     } */
+  });
+
+  const searchButton = document.querySelector('.Search-IconWrapper');
+
+  searchButton.addEventListener('click', function (event) {
+    console.log(document.querySelector('.Search-Form'));
+    console.log('target', event.target);
+    console.log('current', event.currentTarget);
+
+    document.querySelector('.Search-Form').classList.toggle('Search-Form_Disabled');
+  });
+});
