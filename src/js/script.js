cc583cba414dc57942e314230e885db9e7112c7c
gunner7777 <123@yandex.ru>
2019-04-10 16:05:05 +0300

    menu styling

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //first bootstrap
+  //var windowWidth = window.innerWidth;
+  menuControls(window.innerWidth);
+
+  window.addEventListener('resize', function () {
+    menuControls(window.innerWidth);
+  });
+
   document.querySelector('.MobileMenu-Button').addEventListener('click', function (event) {
     var menuButtonClass = "fa-bars";
     var closeButtonClass = "fa-times";
@@ -9,10 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.querySelector('.TopMenu').addEventListener('click', function (event) {
-    console.log('current', event.currentTarget);
-    console.log('target', event.target);
+    //console.log('current', event.currentTarget);
+    //console.log('target', event.target);
     if (event.target.classList.contains('TopMenu-Link') || (event.target.parentNode.classList.contains('TopMenu-Link'))) {
-      console.log(event.target.tagName);
+      //console.log(event.target.tagName);
       if ((event.target.firstElementChild !== null && event.target.firstElementChild.tagName === 'svg') || event.target.tagName === 'svg' || event.target.parentNode.tagName === 'svg') {
         event.target.classList.toggle('TopMenu-Link_Open');
         /* if (event.target.classList.contains('TopMenu-Link_Open')) {
@@ -27,4 +35,24 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   });
+
+
+
+
+
 });
+
+function menuControls(winWidth) {
+  //mouseenter
+  if (winWidth < 449) {
+
+    document.querySelector('.TopMenu').addEventListener('mouseenter', function (event) {
+      console.log('eee', event.target);
+
+      if (event.target.classList.contains('TopMenu-Link')) {
+        event.preventDefault();
+
+      }
+    });
+  }
+}
