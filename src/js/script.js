9496999183e34cc080d086f7d048d4319ec71d89
gunner7777 <123@yandex.ru>
2019-04-11 15:53:30 +0300

    prototype complete

--- src/js/script.js
+++ src/js/script.js
@@ -1,45 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
   //first bootstrap
   var windowWidth = window.innerWidth;
 
-  window.addEventListener('resize', function () {
+  window.addEventListener("resize", function() {
     windowWidth = window.innerWidth;
   });
 
-  document.querySelector('.MobileMenu-Button').addEventListener('click', function (event) {
-    var menuButtonClass = "fa-bars";
-    var closeButtonClass = "fa-times";
+  document
+    .querySelector(".MobileMenu-Button")
+    .addEventListener("click", function(event) {
+      var menuButtonClass = "fa-bars";
+      var closeButtonClass = "fa-times";
 
-    event.currentTarget.firstElementChild.classList.toggle(menuButtonClass);
-    event.currentTarget.firstElementChild.classList.toggle(closeButtonClass);
-    document.querySelector('.TopMenu').classList.toggle('TopMenu_Show');
-  });
+      event.currentTarget.firstElementChild.classList.toggle(menuButtonClass);
+      event.currentTarget.firstElementChild.classList.toggle(closeButtonClass);
+      document.querySelector(".TopMenu").classList.toggle("TopMenu_Show");
+    });
 
-  var menuLinks = document.querySelectorAll('.TopMenu-Link');
+  var menuLinks = document.querySelectorAll(".TopMenu-Link");
   menuLinks.forEach(link => {
-    link.addEventListener('mouseenter', function (event) {
-      if (windowWidth > 449) {
+    link.addEventListener("mouseenter", function(event) {
+      if (windowWidth > 505) {
         var menuLink = event.currentTarget;
         if (menuLink.childNodes.length > 1) {
-          menuLink.nextElementSibling.classList.add('SubMenu_Show');
+          menuLink.nextElementSibling.classList.add("SubMenu_Show");
         }
       }
     });
 
-    link.addEventListener('mouseleave', function (event) {
-      if (windowWidth > 449) {
+    link.addEventListener("mouseleave", function(event) {
+      if (windowWidth > 505) {
         var menuLink = event.currentTarget;
         if (menuLink.childNodes.length > 1) {
-          menuLink.nextElementSibling.classList.remove('SubMenu_Show');
+          menuLink.nextElementSibling.classList.remove("SubMenu_Show");
         }
       }
     });
 
-    link.addEventListener('click', function (event) {
-      if (windowWidth <= 449) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      if (windowWidth <= 505) {
         var menuLink = event.currentTarget;
         if (menuLink.childNodes.length > 1) {
-          menuLink.classList.toggle('TopMenu-Link_Open');
+          menuLink.classList.toggle("TopMenu-Link_Open");
         }
       }
     });
